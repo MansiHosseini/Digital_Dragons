c7b744f2de3ddd3a45b6dce39d94dae788a9f7c5
MansiHosseini <Mansi.hosseini@gmail.com>
2024-12-06 14:34:28 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Klarity_News_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
